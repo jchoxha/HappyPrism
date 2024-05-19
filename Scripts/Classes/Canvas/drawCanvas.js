acb24a4e41971b4b5510d3ad5ad7407493c3258b
Jchoxha <jeton_hoxha@student.uml.edu>
2024-05-19 08:46:00 +0300

    Refactored drawGrid into its own function in drawCanvas.js

--- Scripts/Classes/Canvas/drawCanvas.js
+++ Scripts/Classes/Canvas/drawCanvas.js
@@ -1,35 +1,42 @@
 import { nearestMultiple } from '../../Misc/utils.js';
 
+const gridInterval = 30;
+
 function drawCanvas(canvasManager) {
     canvasManager.ctx.clearRect(0, 0, canvasManager.canvas.width, canvasManager.canvas.height);
     canvasManager.ctx.save();
     canvasManager.ctx.translate(canvasManager.translateX, canvasManager.translateY);
     canvasManager.ctx.scale(canvasManager.scale, canvasManager.scale);
+    drawGrid(canvasManager);
+    canvasManager.nodes.forEach(node => drawNode(canvasManager, node));
+    canvasManager.ctx.restore();
+}
+
+function drawGrid(canvasManager) {
+
     if (canvasManager.topLeftY < 0 && canvasManager.bottomRightY > 0) {
         drawXAxis(canvasManager);
     }
     if (canvasManager.topLeftX < 0 && canvasManager.bottomRightX > 0) {
         drawYAxis(canvasManager);
     }
-    let numNorthSouthLines = Math.floor(canvasManager.visibleWidth / 30);
-    let numEastWestLines = Math.floor(canvasManager.visibleHeight / 30);
-    let startPointX = nearestMultiple(canvasManager.topLeftX, 30);
-    let startPointY = nearestMultiple(canvasManager.topLeftY, 30);
+    let numNorthSouthLines = Math.floor(canvasManager.visibleWidth / gridInterval);
+    let numEastWestLines = Math.floor(canvasManager.visibleHeight / gridInterval);
+    let startPointX = nearestMultiple(canvasManager.topLeftX, gridInterval);
+    let startPointY = nearestMultiple(canvasManager.topLeftY, gridInterval);
 
     for (let i = 0; i <= numNorthSouthLines; i++) {
         canvasManager.ctx.beginPath();
-        canvasManager.ctx.moveTo(startPointX + i * 30, canvasManager.topLeftY);
-        canvasManager.ctx.lineTo(startPointX + i * 30, canvasManager.bottomRightY);
+        canvasManager.ctx.moveTo(startPointX + i * gridInterval, canvasManager.topLeftY);
+        canvasManager.ctx.lineTo(startPointX + i * gridInterval, canvasManager.bottomRightY);
         canvasManager.ctx.stroke();
     }
     for (let i = 0; i <= numEastWestLines; i++) {
         canvasManager.ctx.beginPath();
-        canvasManager.ctx.moveTo(canvasManager.topLeftX, startPointY + i * 30);
-        canvasManager.ctx.lineTo(canvasManager.bottomRightX, startPointY + i * 30);
+        canvasManager.ctx.moveTo(canvasManager.topLeftX, startPointY + i * gridInterval);
+        canvasManager.ctx.lineTo(canvasManager.bottomRightX, startPointY + i * gridInterval);
         canvasManager.ctx.stroke();
     }
-    canvasManager.nodes.forEach(node => drawNode(canvasManager, node));
-    canvasManager.ctx.restore();
 }
 
 function drawXAxis(canvasManager) {
