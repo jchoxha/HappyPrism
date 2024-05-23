82121eac9a13a7d19787c356d27beb7f5853c4a8
Jchoxha <jeton_hoxha@student.uml.edu>
2024-05-23 06:48:40 +0300

    Updated image file reference to work on github

--- Scripts/UI/ui.js
+++ Scripts/UI/ui.js
@@ -23,7 +23,7 @@ function loadUIDefault(){
   <div id="top-bar">
     <div id="top-left">
       <button id="home-button">
-        <img src="/Assets/Images/Icons/ui/Button_Home/LogoDarkLargeNoBG.svg" alt="Home">
+        <img src="Assets/Images/Icons/ui/Button_Home/LogoDarkLargeNoBG.svg" alt="Home">
         <span id="app-name">
         <span class="app-name-letter">H</span>
         <span class="app-name-letter">a</span>
@@ -51,51 +51,51 @@ function loadUIDefault(){
     </div>
     <div id="top-right">
       <button id="search-button">
-      <img src="/Assets/Images/Icons/ui/Button_Search/search.svg" alt="Search">
+      <img src="Assets/Images/Icons/ui/Button_Search/search.svg" alt="Search">
       </button>
       <button id="canvas-menu-button">
-        <img src="/Assets/Images/Icons/ui/Button_Canvas_Menu/menu.svg" alt="Canvas Menu">
+        <img src="Assets/Images/Icons/ui/Button_Canvas_Menu/menu.svg" alt="Canvas Menu">
       </button>
       <button id="user-profile-button">
-        <img src="/Assets/Images/Icons/ui/Button_Profile/profile-circle.svg" alt="User Profile" class="circle-image">
+        <img src="Assets/Images/Icons/ui/Button_Profile/profile-circle.svg" alt="User Profile" class="circle-image">
       </button>
     </div>
   </div>
   <div id="lower-bar">
     <div id="lower-left">
       <button id="undo-button">
-      <img src="/Assets/Images/Icons/ui/Button_Undo/undo.svg" alt="Undo">
+      <img src="Assets/Images/Icons/ui/Button_Undo/undo.svg" alt="Undo">
       </button>
       <button id="redo-button">
-      <img src="/Assets/Images/Icons/ui/Button_Redo/redo.svg" alt="Redo">
+      <img src="Assets/Images/Icons/ui/Button_Redo/redo.svg" alt="Redo">
       </button>
     </div>
     <div id="lower-center">
       <button id="select-or-drag-button">
-      <img src="/Assets/Images/Icons/ui/Button_Cursor/cursor.svg" alt="Select">
+      <img src="Assets/Images/Icons/ui/Button_Cursor/cursor.svg" alt="Select">
       </button>
       <button id="add-shape-button">
-      <img src="/Assets/Images/Icons/ui/Button_Shape/circle.svg" alt="Add Shape">
+      <img src="Assets/Images/Icons/ui/Button_Shape/circle.svg" alt="Add Shape">
       </button>
       <button id="add-text-button">
-      <img src="/Assets/Images/Icons/ui/Button_Text/Text.svg" alt="Add Text">
+      <img src="Assets/Images/Icons/ui/Button_Text/Text.svg" alt="Add Text">
       </button>
       <button id="draw-erase-button">
-      <img src="/Assets/Images/Icons/ui/Button_Draw/pencil.svg" alt="Draw or Erase">
+      <img src="Assets/Images/Icons/ui/Button_Draw/pencil.svg" alt="Draw or Erase">
       </button>
       <button id="add-other-button">
-      <img src="/Assets/Images/Icons/ui/Button_Apps/apps.svg" alt="Add Other">
+      <img src="Assets/Images/Icons/ui/Button_Apps/apps.svg" alt="Add Other">
       </button>
     </div>
     <div id="lower-right">
       <button id="mini-map-button">
-      <img src="/Assets/Images/Icons/ui/Button_Map/map.svg" alt="Map">
+      <img src="Assets/Images/Icons/ui/Button_Map/map.svg" alt="Map">
       </button>
       <button id="zoom-out-button">
-      <img src="/Assets/Images/Icons/ui/Button_Zoom/minus.svg" alt="Zoom Out">
+      <img src="Assets/Images/Icons/ui/Button_Zoom/minus.svg" alt="Zoom Out">
       </button>
       <button id="zoom-in-button">
-      <img src="/Assets/Images/Icons/ui/Button_Zoom/plus.svg" alt="Zoom In">
+      <img src="Assets/Images/Icons/ui/Button_Zoom/plus.svg" alt="Zoom In">
       </button>
     </div>
   </div>
